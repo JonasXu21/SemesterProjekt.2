65dd7ffb58b09b1236c0fb1f42ec88f5f27aa9c9
Dlovan <dlo.sy@outlook.com>
2022-03-08 10:35:31 +0100

    basicStopWordProgram

--- main/src/Stopwords.java
+++ main/src/Stopwords.java
@@ -2,21 +2,37 @@ import java.util.ArrayList;
 
 public class Stopwords {
 
-private ArrayList<String> arraylistStopwords = new ArrayList<>();
-
+private  ArrayList<String> arraylistStopwords ;
 private String searchWord;
 
+    public Stopwords(String searchWord){
+        this.searchWord= searchWord;
+        arraylistStopwords=new ArrayList<>();
+    }
 
 
-    public String displayString(){ return null;}
 
-    public String displayRemovedWords(){ return null;}
+    public String displayString(){
+        return searchWord;
+    }
 
-    public String addStopword(String stopword){return null;}
+    public void displayRemovedWords(){
+       if(arraylistStopwords.contains(searchWord)){
+           displayString();
+       }
+    }
 
-    public String removeStopword (String stopword){return null;}
+    public void addStopword(String stopword){
+        arraylistStopwords.add(stopword);
+    }
 
-    public void compareString() {}
+    public void removeStopword (String stopword){
+        arraylistStopwords.remove(stopword);
+    }
+
+    public void compareString() {
+
+    }
 
 
 
