023f6a7277cd2e15312496694d41f36ca45a6f0b
Miffan20 <92362718+Miffan20@users.noreply.github.com>
2022-03-08 09:53:33 +0100

    setup methods for stopwords class

---
+++ main/src/Stopwords.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Stopwords {
+
+private ArrayList<String> arraylistStopwords = new ArrayList<>();
+
+private String searchWord;
+
+
+
+    public String displayString(){ return null;}
+
+    public String displayRemovedWords(){ return null;}
+
+    public String addStopword(String stopword){return null;}
+
+    public String removeStopword (String stopword){return null;}
+
+    public void compareString() {}
+
+
+
+}
