179fd9e540bfc18dfa84bbea3c29fd1d27c46f55
Miffan20 <92362718+Miffan20@users.noreply.github.com>
2022-03-08 12:11:42 +0100

    Update Stopwords.java

--- main/src/Stopwords.java
+++ main/src/Stopwords.java
@@ -4,77 +4,72 @@ public class Stopwords {
 
 
     private Collection<String> TreesetStopwords;
-private String searchWord;
+    private String searchWord;
 
-    public Stopwords(){
-        this.searchWord= searchWord;
+    public Stopwords() {
+        this.searchWord = searchWord;
         TreesetStopwords = new TreeSet<>();
 
     }
 
 
-    public String addSearchword(String searchWord){
-      return this.searchWord=searchWord;
+    public String addSearchword(String searchWord) {
+        return this.searchWord = searchWord;
     }
 
 
-    public String displayString(){
+    public String displayString() {
         return searchWord;
 
     }
 
-    public void displayRemovedWords(){
-       if(TreesetStopwords.contains(searchWord)){
-           displayString();
-       }
+    public void displayRemovedWords() {
+        if (TreesetStopwords.contains(searchWord)) {
+            displayString();
+        }
     }
 
-    public void addStopword(String stopword){
+    public void addStopword(String stopword) {
         TreesetStopwords.add(stopword);
     }
 
-    public void removeStopword (String stopword){
+    public void removeStopword(String stopword) {
         TreesetStopwords.remove(stopword);
     }
 
-    public void compareString() {
+    public void compareString(String searhWord) {
 
+        this.searchWord = searhWord;
         String[] words = searchWord.split("\\W+");
 
-        List<String> wordslist = new ArrayList<String>(Arrays.asList(words));
 
+        List<String> wordslist = new ArrayList<>(Arrays.asList(words));
 
+        System.out.println(wordslist);
+        System.out.println(TreesetStopwords);
 
-        for(int i = 0; i<wordslist.size(); i++){
-            for(int j = 0; j<TreesetStopwords.size();j++)
-            if( words[i].equals(TreesetStopwords[j] ){
-                wordslist.remove(i);
+        for (int i = 0; i < wordslist.size(); i++) {
+                if(wordslist.get(i).equals(TreesetStopwords.toArray()[i])){
+                    wordslist.remove(i);
+                }
+
             }
-        }
+        System.out.println(wordslist);
 
     }
+
+
 
     public static void main(String[] args) {
 
         Stopwords sw = new Stopwords();
 
-        sw.addStopword("where");
-        sw.addStopword("where");
         sw.addStopword("more");
+        sw.addStopword("where");
 
-        System.out.println(sw.TreesetStopwords);
 
-        sw.removeStopword("where");
 
-        System.out.println(sw.TreesetStopwords);
-
-        sw.addSearchword("where more products");
-
-        System.out.println(sw.displayString());
-
-        sw.compareString();
-
-        System.out.println(sw.displayString());
+        sw.compareString("more where products");
 
 
     }
