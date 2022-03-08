5d353e23d4ab7ade769af07ae7ea3f019c1fff8e
Miffan20 <92362718+Miffan20@users.noreply.github.com>
2022-03-08 11:26:29 +0100

    Update stopwords

--- main/src/Stopwords.java
+++ main/src/Stopwords.java
@@ -1,39 +1,81 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Stopwords {
 
-private  ArrayList<String> arraylistStopwords ;
+
+    private Collection<String> TreesetStopwords;
 private String searchWord;
 
-    public Stopwords(String searchWord){
+    public Stopwords(){
         this.searchWord= searchWord;
-        arraylistStopwords=new ArrayList<>();
+        TreesetStopwords = new TreeSet<>();
+
     }
 
 
+    public String addSearchword(String searchWord){
+      return this.searchWord=searchWord;
+    }
 
     public String displayString(){
         return searchWord;
+
     }
 
     public void displayRemovedWords(){
-       if(arraylistStopwords.contains(searchWord)){
+       if(TreesetStopwords.contains(searchWord)){
            displayString();
        }
     }
 
     public void addStopword(String stopword){
-        arraylistStopwords.add(stopword);
+        TreesetStopwords.add(stopword);
     }
 
     public void removeStopword (String stopword){
-        arraylistStopwords.remove(stopword);
+        TreesetStopwords.remove(stopword);
     }
 
     public void compareString() {
 
+        String[] words = searchWord.split("\\W+");
+
+        List<String> wordslist = new ArrayList<String>(Arrays.asList(words));
+
+
+
+        for(int i = 0; i<wordslist.size(); i++){
+            for(int j = 0; j<TreesetStopwords.size();j++)
+            if( words[i].equals(TreesetStopwords[j] ){
+                wordslist.remove(i);
+            }
+        }
+
     }
 
+    public static void main(String[] args) {
 
+        Stopwords sw = new Stopwords();
+
+        sw.addStopword("where");
+        sw.addStopword("where");
+        sw.addStopword("more");
+
+        System.out.println(sw.TreesetStopwords);
+
+        sw.removeStopword("where");
+
+        System.out.println(sw.TreesetStopwords);
+
+        sw.addSearchword("where more products");
+
+        System.out.println(sw.displayString());
+
+        sw.compareString();
+
+        System.out.println(sw.displayString());
+
+
+    }
 
 }
